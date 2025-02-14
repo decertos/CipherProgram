ef7b6ba5e666530aa46689fbd37740035e75fbe9
decert <dyadenkodmitriy1@yandex.ru>
2025-02-14 21:45:00 +0400

    Added more ciphers

--- main_file.py
+++ main_file.py
@@ -4,7 +4,57 @@ from itertools import cycle
 from PyQt6 import uic
 import sqlite3
 from os.path import isfile
+from uis.primes import prime_numbers
+from random import randint
 import pyperclip
+
+
+def bin_pow(base, exponent, modulus):
+    result = 1
+    base = base % modulus
+    while exponent > 0:
+        if exponent % 2 == 1:
+            result = (result * base) % modulus
+        exponent //= 2
+        base = (base * base) % modulus
+    return result
+
+
+def extended_gcd(a, b):
+    if a == 0:
+        return b, 0, 1
+    else:
+        gcd, x1, y1 = extended_gcd(b % a, a)
+        x = y1 - (b // a) * x1
+        y = x1
+        return gcd, x, y
+
+
+def mod_inverse(e, m):
+    gcd, x, y = extended_gcd(e, m)
+    if gcd != 1:
+        return None
+    else:
+        return x % m
+
+
+def gcd(a, b):
+    if a < b:
+        a, b = b, a
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    d = 2
+    while d * d <= n:
+        if n % d == 0:
+            return False
+        d += 1
+    return True
 
 
 class MainWindow(QMainWindow):
@@ -16,6 +66,8 @@ class MainWindow(QMainWindow):
         self.vizhener.clicked.connect(self.vizhener_open)
         self.vizhener_key.clicked.connect(self.vizhener_decrypt_open)
         self.polibium.clicked.connect(self.polibium_open)
+        self.rails.clicked.connect(self.rails_open)
+        self.rsa.clicked.connect(self.rsa_open)
         self.alphabets.clicked.connect(self.alphabets_open)
 
         created = True
@@ -56,6 +108,14 @@ class MainWindow(QMainWindow):
     def polibium_open(self):
         self.polibiumWindow = PolibiumBoard()
         self.polibiumWindow.show()
+
+    def rails_open(self):
+        self.railsWindow = RailsCipher()
+        self.railsWindow.show()
+
+    def rsa_open(self):
+        self.rsaWindow = RSACipher()
+        self.rsaWindow.show()
 
     def alphabets_open(self):
         self.alphabetsWindow = AlphabetsMenu()
@@ -534,6 +594,200 @@ class DeleteAlphabet(QWidget):
 
     def cancel_adding(self):
         self.close()
+
+
+class RailsCipher(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("uis/rails.ui", self)
+        self.cipher_button.clicked.connect(self.to_cipher)
+        self.decipher_button.clicked.connect(self.to_decipher)
+
+        self.deciphered.textChanged.connect(self.onValueChange)
+        self.ciphered.textChanged.connect(self.onValueChange)
+
+    def to_cipher(self):
+        text = self.deciphered.toPlainText()
+        key = self.key.value()
+        if key == 1:
+            self.ciphered.setPlainText(text)
+            return
+        result = ""
+        for row in range(key):
+            index = row
+            is_odd_step = True
+            while index < len(text):
+                result += text[index]
+                if row != 0 and row != key - 1:
+                    if is_odd_step:
+                        index += (key - row - 1) * 2
+                    else:
+                        index += row * 2
+                    is_odd_step = not is_odd_step
+                else:
+                    index += (key - 1) * 2
+        self.ciphered.setPlainText(result)
+
+    def to_decipher(self):
+        text = self.ciphered.toPlainText()
+        key = self.key.value()
+        if key == 1:
+            self.deciphered.setPlainText(text)
+            return
+        result = ""
+        matrix = [['' for _ in range(len(text))] for _ in range(key)]
+        direction = 1
+        row, col = 0, 0
+        for _ in range(len(text)):
+            matrix[row][col] = '*'
+            row += direction
+            if row == key - 1 or row == 0:
+                direction *= -1
+            col += 1
+        index = 0
+        for i in range(key):
+            for j in range(len(text)):
+                if matrix[i][j] == '*' and index < len(text):
+                    matrix[i][j] = text[index]
+                    index += 1
+        plaintext = ''
+        direction = 1
+        row, col = 0, 0
+        for _ in range(len(text)):
+            plaintext += matrix[row][col]
+            row += direction
+            if row == key - 1 or row == 0:
+                direction *= -1
+            col += 1
+        self.deciphered.setPlainText(plaintext)
+
+    def onValueChange(self):
+        self.key.setMaximum(max(len(self.ciphered.toPlainText()), len(self.deciphered.toPlainText())))
+
+
+class RSACipher(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("uis/rsa.ui", self)
+        self.keys_generator.clicked.connect(self.open_keys_generator)
+
+        self.cipher_button.clicked.connect(self.cipher)
+        self.decipher_button.clicked.connect(self.decipher)
+
+    def cipher(self):
+        def rsa_encrypt(character_code, public_key):
+            return bin_pow(character_code, public_key[0], public_key[1])
+
+        self.infoLabel.setText("")
+
+        text = self.deciphered.toPlainText()
+        public_key = [self.e_input.text(), self.n_input_1.text()]
+        if not all(public_key[i].isdigit() for i in range(2)):
+            self.infoLabel.setText("Числа e и n должны быть числами")
+            return
+        public_key = list(map(int, public_key))
+        selected_text = self.textButton.isChecked()
+        if not selected_text:
+            text = text.split()
+
+        encrypted_result = []
+        for character in text:
+            try:
+                character_code = ord(character) if selected_text else int(character)
+            except Exception as e:
+                self.infoLabel.setText("Выбран режим 'числа', в тексте не всё - числа")
+                return
+            encrypted_result.append(rsa_encrypt(character_code, public_key))
+        self.ciphered.setPlainText(" ".join(map(str, encrypted_result)))
+
+    def decipher(self):
+        def rsa_decrypt(character, closed_key):
+            ans = bin_pow(character, closed_key[0], closed_key[1])
+            if selected_text:
+                return chr(ans)
+            return str(ans)
+
+        self.infoLabel.setText("")
+
+        code = self.ciphered.toPlainText().split()
+        closed_key = [self.d_input.text(), self.n_input_2.text()]
+        if not all(closed_key[i].isdigit() for i in range(2)):
+            self.infoLabel.setText("Числа d и n должны быть числами")
+            return
+        closed_key = list(map(int, closed_key))
+        selected_text = self.textButton.isChecked()
+        decrypted_result = ""
+        for character in code:
+            try:
+                decrypted_result += rsa_decrypt(int(character) if selected_text else int(character), closed_key)
+                if not selected_text:
+                    decrypted_result += " "
+            except Exception as e:
+                self.infoLabel.setText("Не всё в тексте - числа или не получилось преобразовать в текст")
+                return
+        if not selected_text:
+            decrypted_result = decrypted_result[:-1]
+        self.deciphered.setPlainText(decrypted_result)
+
+    def open_keys_generator(self):
+        self.keysGeneratorWindow = RSAKeysGenerator()
+        self.keysGeneratorWindow.show()
+
+
+class RSAKeysGenerator(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("uis/rsa_generator.ui", self)
+        self.random_p.clicked.connect(self.random_p_generate)
+        self.random_q.clicked.connect(self.random_q_generate)
+
+        self.generate_button.clicked.connect(self.generate)
+
+        self.copy_open_key.clicked.connect(self.copy_open_key_action)
+        self.copy_closed_key.clicked.connect(self.copy_closed_key_action)
+
+    def random_p_generate(self):
+        self.p_input.setText(str(prime_numbers[randint(0, len(prime_numbers))]))
+
+    def random_q_generate(self):
+        self.q_input.setText(str(prime_numbers[randint(0, len(prime_numbers))]))
+
+    def generate(self):
+        self.infoLabel.setText("")
+
+        p, q = self.p_input.text(), self.q_input.text()
+        if not (p.isdigit() and q.isdigit()):
+            self.infoLabel.setText("Числа p и q должны быть числами")
+            return
+        if p == q:
+            self.infoLabel.setText("Числа p и q должны быть различными")
+            return
+
+        p, q = int(p), int(q)
+        if not (is_prime(p) and is_prime(q)):
+            self.infoLabel.setText("Числа p и q должны быть простыми")
+            return
+
+        n = p * q
+        euler = (p - 1) * (q - 1)
+        for i in range(17, euler):
+            if gcd(euler, i) == 1:
+                opened_number = i
+                break
+        else:
+            opened_number = prime_numbers[randint(0, len(prime_numbers) - 1)]
+        closed_number = mod_inverse(opened_number, euler)
+
+        self.open_key_text.setText(f"{opened_number} {n}")
+        self.closed_key_text.setText(f"{closed_number} {n}")
+
+    def copy_open_key_action(self):
+        open_key = self.open_key_text.text()
+        pyperclip.copy(open_key)
+
+    def copy_closed_key_action(self):
+        closed_key = self.closed_key_text.text()
+        pyperclip.copy(closed_key)
 
 
 if __name__ == "__main__":
